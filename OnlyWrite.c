46cd0d0244aebf3e60295eb893735c8dc8bcea50
computerteacher11 <santoshgaire97@gmail.com>
2025-02-18 14:23:05 +0545

    Add files via upload

---
+++ OnlyWrite.c
@@ -0,0 +1,42 @@
+/*Program to create a data file "student.txt" 
+then store name and marks of n students in the data file.*/
+
+#include <stdio.h>
+
+int main()
+
+{
+
+char name [50];
+
+int marks, i, num;
+
+printf("Enter number of students: ");
+
+scanf("%d", &num);
+
+FILE *fptr;
+
+fptr= (fopen("student.txt", "w"));
+
+for(i=0; i<num; ++i)
+
+{
+
+printf("Enter name:");
+
+scanf("%s", name);
+
+printf("Enter marks:");
+
+scanf("%d", &marks);
+
+fprintf(fptr,"\nName: %s \nMarks=%d \n", name, marks);
+
+}
+
+fclose(fptr);
+
+return 0;
+
+}
